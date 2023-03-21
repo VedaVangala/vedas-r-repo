eeaf289689e0cb9d92cd346f4626c0c41f5f7319
VedaVangala <122326450+VedaVangala@users.noreply.github.com>
2023-03-21 02:45:02 -0400

    Add files via upload

---
+++ R10/sum.R
@@ -0,0 +1,17 @@
+#' Add two numbers together
+#'
+#' This function takes two arguments, `y` and `z`, and returns their sum.
+#'
+#' @param y a numeric value to be added
+#' @param z a numeric value to be added
+#'
+#' @return the sum of `y` and `z`
+#'
+#' @examples
+#' s(2, 3)
+#'
+#' @export
+s = function(y, z) {
+  result = y + z
+  return(result)
+}

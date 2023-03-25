c8bc1f49808cf48da47fbe2c99808bab9b61ff6d
VedaVangala <122326450+VedaVangala@users.noreply.github.com>
2023-03-25 18:40:17 -0400

    Add files via upload

---
+++ R11/tukey_multiple.R
@@ -0,0 +1,13 @@
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE,dim=dim(x))
+  for (j in 1:ncol(x))
+  {
+    outliers[,j] <- outliers[,j] 
+  }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x))
+  { outlier.vec[i] <- all(outliers[i,]) } 
+  return(outlier.vec) }
+matrix <- matrix(rnorm(12), nrow = 3, ncol = 4)
+debug(tukey_multiple)
+tukey_multiple(matrix)
